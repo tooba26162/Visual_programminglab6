4bfc1c819a4834c62e4f9be75b5e02c085073c6f
tooba26162 <233518@students.au.edu.pk>
2024-11-12 17:11:49 +0500

    Add files via upload

---
+++ lab_06_task/lab_06_task/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace lab_06_task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddItems = new System.Windows.Forms.Button();
+            this.RemoveItem = new System.Windows.Forms.Button();
+            this.DisplayItems = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // AddItems
+            // 
+            this.AddItems.Location = new System.Drawing.Point(198, 90);
+            this.AddItems.Name = "AddItems";
+            this.AddItems.Size = new System.Drawing.Size(101, 23);
+            this.AddItems.TabIndex = 0;
+            this.AddItems.Text = "Add Items";
+            this.AddItems.UseVisualStyleBackColor = true;
+            this.AddItems.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RemoveItem
+            // 
+            this.RemoveItem.Location = new System.Drawing.Point(504, 90);
+            this.RemoveItem.Name = "RemoveItem";
+            this.RemoveItem.Size = new System.Drawing.Size(102, 23);
+            this.RemoveItem.TabIndex = 1;
+            this.RemoveItem.Text = "Remove  Items";
+            this.RemoveItem.UseVisualStyleBackColor = true;
+            this.RemoveItem.Click += new System.EventHandler(this.RemoveItem_Click);
+            // 
+            // DisplayItems
+            // 
+            this.DisplayItems.Location = new System.Drawing.Point(348, 90);
+            this.DisplayItems.Name = "DisplayItems";
+            this.DisplayItems.Size = new System.Drawing.Size(105, 23);
+            this.DisplayItems.TabIndex = 2;
+            this.DisplayItems.Text = "Display Items";
+            this.DisplayItems.UseVisualStyleBackColor = true;
+            this.DisplayItems.Click += new System.EventHandler(this.DisplayItems_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(353, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(353, 166);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.DisplayItems);
+            this.Controls.Add(this.RemoveItem);
+            this.Controls.Add(this.AddItems);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddItems;
+        private System.Windows.Forms.Button RemoveItem;
+        private System.Windows.Forms.Button DisplayItems;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ lab_06_task/lab_06_task/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_06_task
+{
+    public partial class Form1 : Form
+    {
+        private List<string> itemsList = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string item = textBox1.Text;
+            if (!string.IsNullOrEmpty(item))
+            {
+                itemsList.Add(item);
+                textBox1.Clear();
+                MessageBox.Show("Item added!");
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid item.");
+            }
+        }
+
+        private void DisplayItems_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (string item in itemsList)
+            {
+                listBox1.Items.Add(item);
+            }
+
+        }
+
+        private void RemoveItem_Click(object sender, EventArgs e)
+        {
+            string item = textBox1.Text;
+            if (itemsList.Contains(item))
+            {
+                itemsList.Remove(item);
+                MessageBox.Show("Item removed!");
+            }
+            else
+            {
+                MessageBox.Show("Item not found.");
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
